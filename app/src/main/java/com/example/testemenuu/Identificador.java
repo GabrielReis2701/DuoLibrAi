38786d0afaab5490c383ec8fa82e4c90f03fcf37
GabrielReis2701 <gabrielreis2701@gmail.com>
2023-08-30 22:22:53 -0400

    Primeira versão Pronta

---
+++ app/src/main/java/com/example/testemenuu/Identificador.java
@@ -0,0 +1,98 @@
+package com.example.testemenuu;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.InterpreterApi;
+import org.tensorflow.lite.InterpreterFactory;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.common.TensorProcessor;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.label.TensorLabel;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.util.List;
+import java.util.Map;
+
+public class Identificador {
+    private Context context;
+    final String ASSOCIATED_AXIS_LABELS = "labels.txt";
+    List<String> associatedAxisLabels = null;
+    String letraComMaiorPrecisao="";
+
+    public Identificador(Context context) {
+        this.context = context;
+    }
+
+    public String classificarImagem(Bitmap image){
+        ImageProcessor imageProcessor =
+                new ImageProcessor.Builder()
+                        .add(new ResizeOp(64, 64, ResizeOp.ResizeMethod.BILINEAR))
+                        .add(new NormalizeOp(0.0f,255.0f))
+                        .build();
+
+        // Create a TensorImage object. This creates the tensor of the corresponding
+        // tensor type (uint8 in this case) that the TensorFlow Lite interpreter needs.
+        TensorImage tensorImage = new TensorImage(DataType.FLOAT32);
+
+        // Analysis code for every frame
+        // Preprocess the image
+        tensorImage.load(image);
+        tensorImage = imageProcessor.process(tensorImage);
+
+        // Create a container for the result and specify that this is a quantized model.
+// Hence, the 'DataType' is defined as UINT8 (8-bit unsigned integer)
+        TensorBuffer probabilityBuffer =
+                TensorBuffer.createFixedSize(new int[]{1, 21}, DataType.FLOAT32);
+        // Initialise the model
+        InterpreterApi tflite = null;
+        try {
+            MappedByteBuffer tfliteModel;
+            tfliteModel = FileUtil.loadMappedFile(context, "modeloClassificadorSinais.tflite");
+            tflite = new InterpreterFactory().create(
+                    tfliteModel, new InterpreterApi.Options());
+        } catch (IOException e) {
+            Log.e("tfliteSupport", "Error reading model", e);
+        }
+
+// Running inference
+        if (null != tflite) {
+            tflite.run(tensorImage.getBuffer(), probabilityBuffer.getBuffer());
+        }
+
+
+        try {
+            associatedAxisLabels = FileUtil.loadLabels(context, ASSOCIATED_AXIS_LABELS);
+        } catch (IOException e) {
+            Log.e("tfliteSupport", "Error reading label file", e);
+        }
+        // Post-processor which dequantize the result
+        TensorProcessor probabilityProcessor =
+                new TensorProcessor.Builder().add(new NormalizeOp(0, 255)).build();
+
+        if (null != associatedAxisLabels) {
+            // Map of labels and their corresponding probability
+            TensorLabel labels = new TensorLabel(associatedAxisLabels,
+                    probabilityProcessor.process(probabilityBuffer));
+
+            // Create a map to access the result based on label
+            Map<String, Float> floatMap = labels.getMapWithFloatValue();
+            Map.Entry<String, Float> maxEntry = null;
+            for (Map.Entry<String, Float> entry : floatMap.entrySet()) {
+                if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
+                    maxEntry = entry;
+                }
+            }
+            letraComMaiorPrecisao = maxEntry.getKey();
+            System.out.println(letraComMaiorPrecisao);
+        }
+        return letraComMaiorPrecisao;
+    }
+}
--- app/src/main/java/com/example/testemenuu/Letras.java
+++ app/src/main/java/com/example/testemenuu/Letras.java
@@ -1,14 +1,85 @@
 package com.example.testemenuu;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class Letras extends AppCompatActivity {
+    String letraEscolhida = "", letraFeita = "";
+    Identificador identificador = new Identificador(this);
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap imageBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_letras);
     }
+
+    public void onclick(View button) {
+        letraEscolhida = button.getTag().toString();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setMessage("Faça o sinal de acordo com a imagem abaixo")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        tirarFoto();
+                    }
+                })
+                .setView(createImageView(letraEscolhida));
+        builder.create().show();
+
+    }
+    private View createImageView(String letra) {
+        ImageView imageView = new ImageView(this);
+        String imageName = "letra_" + letra.toLowerCase();
+        int resId = getResources().getIdentifier(imageName, "drawable", getPackageName());
+        imageView.setImageResource(resId);
+        return imageView;
+    }
+
+    private void tirarFoto() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            letraFeita = identificador.classificarImagem(imageBitmap);
+        }
+        comparador();
+    }
+    private void comparador(){
+        ImageView imageView= new ImageView(this);
+        if (letraFeita.equals(letraEscolhida)) {
+            AlertDialog.Builder janela1 = new AlertDialog.Builder(Letras.this);
+            imageView.setImageResource(R.drawable.parabens);
+            janela1.setView(imageView);
+            janela1.setMessage("Letra Feita: " + letraFeita + " Letra Escolhida: " + letraEscolhida);
+            janela1.setNeutralButton("ok", null);
+            janela1.show();
+        } else {
+            AlertDialog.Builder janela2 = new AlertDialog.Builder(Letras.this);
+            imageView.setImageResource(R.drawable.errou);
+            janela2.setView(imageView);
+            janela2.setMessage("Letra Feita: " + letraFeita + " Letra Escolhida: " + letraEscolhida);
+            janela2.setNeutralButton("ok", null);
+            janela2.show();
+        }
+    }
+
 }
